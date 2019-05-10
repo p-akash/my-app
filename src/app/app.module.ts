ee1b77dd96298f5b021cb981b649e9f7d7f93805
p-akash <patel.akash@outlook.com>
2019-05-10 11:28:20 +0530

    assignment 1 complated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { successAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    successAlertComponent
   ],
   imports: [
     BrowserModule,
